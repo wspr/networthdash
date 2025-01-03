958aaea8fefe11f2c26c5fcf362620834ca5bcea
Will Robertson <wspr81@gmail.com>
2025-01-03 09:07:48 +0800

    format dataclass doc properly

--- src/config.py
+++ src/config.py
@@ -4,26 +4,29 @@ from typing import List
 @dataclass
 class Config:
     """
-    Configuration class for networthdash
+    Configuration class for networthdash.
     
-    Attributes:
-        csv (str): path and filename of CSV input file
-
-        savedir (str): path (ending in "/") to save PDF dashboards to
-
-        datefmt (str): the date format of dates used in the CSV file
-        
-        since_yr (int): year to start the dashboard (defaults to earliest year in CSV file)
-        
-        born_yr (int): year you were born
-
-        retire_age (int): year you expect to retire
-        
-        income_thresh (float): threshold between zero and one to identify "major" or "minor" income sources based on the all-time grand totals for each income column.
-        
-        linear_window (float): number of years to linearly extrapolate from
-        
-        anon (bool): if True, hides all numerical labels
+    Parameters
+    ----------
+    csv : str
+        Path and filename of the CSV input file.
+    savedir : str
+        Path (ending in "/") to save PDF dashboards.
+    datefmt : str
+        The date format used in the CSV file.
+    since_yr : int, optional
+        Year to start the dashboard (default is the earliest year in the CSV file).
+    born_yr : int
+        The year you were born.
+    retire_age : int
+        The age at which you expect to retire.
+    income_thresh : float
+        Threshold (between 0 and 1) for identifying "major" or "minor" income sources 
+        based on the all-time grand totals for each income column.
+    linear_window : float
+        Number of years to linearly extrapolate from.
+    anon : bool
+        If True, hides all numerical labels.
     """
     
     csv: str = "net-worth.csv"
