8efa15a1f2bc7453b6de24bc1f2f534f0bc88261
Will Robertson <wspr81@gmail.com>
2025-01-12 17:52:30 +0800

    dont try to save test suite images

--- tests/test_nwd.py
+++ tests/test_nwd.py
@@ -6,11 +6,9 @@ def test_simple():
         csv = "nwd_example.csv" ,
         csvdir = "tests/" ,
         saveprefix = "test_simple" ,
-        savedir = "_site/" ,
         datefmt = "%Y-%m-%d" ,
         born_yr = 1981 ,
         retire_age = 67 ,
-        savepng = True ,
         savepdf = False ,
     )
     nwd.dashboard(cfg)
