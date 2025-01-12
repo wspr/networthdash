3831b14b9670bc9e99f65479d012f8856e5109d8
Will Robertson <wspr81@gmail.com>
2025-01-12 16:06:37 +0800

    ditto

--- tests/test_nwd.py
+++ tests/test_nwd.py
@@ -3,8 +3,8 @@ import networthdash as nwd
 def test_simple():
     cfg = nwd.Config(
         csv = "nwd_example.csv" ,
-        saveprefix = "test_simple"
-        datefmt = "%Y-%m-%d",
+        saveprefix = "test_simple" ,
+        datefmt = "%Y-%m-%d" ,
         born_yr = 1981 ,
         retire_age = 67 ,
     )
