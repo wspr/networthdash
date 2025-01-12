6af9091d8144157900e4921407609b63f4cd2f01
Will Robertson <wspr81@gmail.com>
2025-01-12 15:39:30 +0800

    simplify

--- tests/nwd_tests.py
+++ tests/nwd_tests.py
@@ -1,17 +1,13 @@
 import networthdash as nwd
-import unittest
 
-class GenericTest(unittest.TestCase):
+def test_simple():
+    cfg = nwd.Config(
+        csv = "nwd_example.csv" ,
+        saveprefix = "test_simple"
+        datefmt = "%Y-%m-%d",
+        born_yr = 1981 ,
+        retire_age = 67 ,
+    )
+    nwd.dashboard(cfg)
 
-    """Generic tests for nwd."""
-
-    def test_simple(self):
-        cfg = nwd.Config(
-            csv = "nwd_example.csv" ,
-            saveprefix = "test_simple"
-            datefmt = "%Y-%m-%d",
-            born_yr = 1981 ,
-            retire_age = 67 ,
-        )
-        nwd.dashboard(cfg)
 
